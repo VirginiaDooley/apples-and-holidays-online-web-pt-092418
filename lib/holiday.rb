f9ca1218011d15ddd8053a7943d3ccb367c1c819
Virginia Dooley <vcdooley@gmail.com>
2018-10-05 12:57:51 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,8 +63,8 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, supply|
       if supply.include?("BBQ")
-      end 
-    end 
+      end
+    end
   end
   all_holidays_with_bbq.flatten
 end

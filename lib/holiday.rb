de540a5b84dd27438ba43960065da9fe52c611e4
Virginia Dooley <vcdooley@gmail.com>
2018-10-05 12:53:19 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,6 @@ def all_supplies_in_holidays(holiday_hash)
       holiday.each do |holiday, supply|
         puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{supply.join(", ")}"
       end
-    end
   end
 end
 
